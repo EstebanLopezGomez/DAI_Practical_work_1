6ab47f77a08da61784abfd1c8ef220bd76fe96bc
esteban.lopez <esteban.lopez@heig-vd.ch>
2023-10-04 15:25:10 +0200

    Added lecture of file

--- Practical_Work_1/src/main/java/esteban/lopez/CLI.java
+++ Practical_Work_1/src/main/java/esteban/lopez/CLI.java
@@ -1,24 +1,57 @@
 package esteban.lopez;
 
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import java.io.File;
-import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+
+@Command(name = "CLI",sortOptions = false,headerHeading = "%nUsage of this app:%n%n", footer = "%nCopyright(c) 2023 HEIG-VD - Esteban Lopez",
+        descriptionHeading = "%nDescription : %n",optionListHeading = "%nOptions : %n",commandListHeading = "%nCommands : %n",description = "Modifies the content of a file and saves it on an output file")
 
 public class CLI implements Runnable{
-    @Parameters(paramLabel = "-i",description = "input  file")
+    @Option(names = {"-i","--inputFile"},required = true, description = "Input file where the text will be retrieved")
     File inputFile;
 
-    @Option(names = "-o",description = "output  file")
+    //@Option(names = {"-ie","-inputFileEncoding"},defaultValue = "UTF-8", description = "Defines the encoding of the input file, default : ")
+
+
+
+    @Option(names = {"-o","--outputFile"}, required = true, defaultValue = "output.txt",description = "Output file where the modified text will be saved. Default  value = {DEFAULT_VALUE}")
     File outputFile;
+
+    @Option(names = "uppercase", description = "Sets the text on the input file to Uppercase and saves it on the output file")
+    private boolean toUppercase;
+
+    @Option(names = "lowercase", description = "Sets the text on the input file to Lowercase and saves it on the output file")
+    private boolean toLowercase;
 
     @Option(names = { "-h", "--help" }, usageHelp = true, description = "display a help message")
     private boolean helpRequested;
 
+
+
     @Override
     public void run(){
         System.out.println("test");
+
+        try {
+            Scanner scanner = new Scanner(inputFile);
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+
+                System.out.println(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void putToUppercase(){
     }
 
 
--- Practical_Work_1/src/main/java/esteban/lopez/Main.java
+++ Practical_Work_1/src/main/java/esteban/lopez/Main.java
@@ -5,7 +5,6 @@ import picocli.CommandLine;
 public class Main {
     public static void main(String[] args) {
 
-
         int exitCode = new CommandLine(new CLI()).execute(args);
         System.exit(exitCode);
 
